676b3a947925941aca1a748aaf9bb21d1f5f163b
tonamuhang <39136949+tonamuhang@users.noreply.github.com>
2019-09-12 12:37:15 -0400

    Add position vector for loading from xml

--- src/comp557/a1/SphericalJoint.java
+++ src/comp557/a1/SphericalJoint.java
@@ -2,6 +2,7 @@ package comp557.a1;
 
 import javax.vecmath.Tuple3d;
 
+import com.jogamp.openal.sound3d.Vec3f;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 
@@ -11,13 +12,14 @@ public class SphericalJoint extends GraphNode{
 	DoubleParameter rx;
 	DoubleParameter ry;
 	DoubleParameter rz;
-	Tuple3d position;
+	Vec3f position;
 	
 	public SphericalJoint(String name, DoubleParameter rx, DoubleParameter ry, DoubleParameter rz) {
 		super(name);
 		this.rx = rx;
 		this.ry = ry;
 		this.rz = rz;
+		this.position = new Vec3f(0, 0, 0);
 		dofs.add(rx);
 		dofs.add(ry);
 		dofs.add(rz);
@@ -25,7 +27,8 @@ public class SphericalJoint extends GraphNode{
 	}
 	
 	public void setPosition(Tuple3d position) {
-		this.position = position;
+		this.position = new Vec3f((float)position.x, (float)position.y, (float)position.z);
+		
 	}
 	@Override
 	public void display(GLAutoDrawable drawable) {
@@ -37,7 +40,7 @@ public class SphericalJoint extends GraphNode{
 		gl.glRotated(rx.getValue(), 1, 0, 0);
 		gl.glRotated(ry.getValue(), 0, 1, 0);
 		gl.glRotated(rz.getValue(), 0, 0, 1);
-		gl.glTranslated(position.x, position.y, position.z);//Temp
+		gl.glTranslated(position.v1, position.v2, position.v3);//Temp
 		
 		super.display(drawable);
 		gl.glPopMatrix();
